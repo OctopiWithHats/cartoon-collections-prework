f366072dd33db2b70db432104cc304cec2186821
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-21 01:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,12 +20,12 @@ def long_planeteer_calls(calls)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(stuff)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   i=0
   while i<cheese_types.length
-    return cheese_types[i] if array.include?(cheese_types[i])
+    return cheese_types[i] if stuff.include?(cheese_types[i])
     i += 1
   end
 end
