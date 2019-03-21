2bf551a0b76fac7a904bd5ad6bf6a48ef8aeb2ac
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-21 01:30:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,5 +25,5 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i=0
   while i<cheese_types.length
-    return cheese
-end
+return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1end
